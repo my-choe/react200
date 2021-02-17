49a8383bb8705416a76fff2f0145ff560668d812
minyoung choi <my_choe@naver.com>
2021-02-17 23:31:24 +0900

    002 .jsx에 css 적용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>HTML 적용하기</p>
+      <p>CSS 적용하기</p>
     </div>
   );
 }
