27f8ed62bc1c4b59f48dcc3ae0f6ab2c0f3b6b89
minyoung choi <my_choe@naver.com>
2021-02-17 23:38:14 +0900

    Component 사용하기

---
+++ src/P003_ImportComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class R003_ImportComponent extends Component {
+   render() {
+       return (
+           <h2>[THIS IS IMPORTED COMPONENT]</h2>
+       )
+   }
+}
+
+export default R003_ImportComponent;
