724f8e2f476b569c50898358b8c3bc27434a04c4
my_choe <my_choe@naver.com>
2021-03-24 15:41:44 +0900

    083 react-redux로 스토어 데이터 변경하기

--- src/StrAddButton.js
+++ src/StrAddButton.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {add} from './actions';
 /**
- * dispatch함수를 통해 add함수(actions 폴더의 index.js 파일)의 반환값을 스토어에 전달한다.
- * 이때 add 함수의 type 값을 리듀서(reducers 폴더의 index.js)에서 참고해 스토어 데이터를 변경한다.
- * 결론적으로 dispatch함수는 reducer에 액션을 전달하고
- * 리듀서에는 액션에 작성된 작업 내용을 읽어 스토어 데이터를 변경한다.
+ * connect 함수의 두 번째 파라미터 mapDispatchToProps 함수로,
+ * dispatch 함수를 컴포넌트 함수에 바인딩할 수 있다.
+ * 즉, 컴포넌트 함수가 실행되면 바인딩 된 dispatch 함수가 실행된다.
  */
 class StrAddButton extends Component {
     render() {
         return (
-            <input value="Add200" type="button" onClick={this.addString}/>
+            /* <input value="Add200" type="button" onClick={this.addString}/> */
+            <input value='Add200' type="button" onClick={this.props.addString} />
         )
     }
 
-    addString = () => {
+    // line26에서 두 함수를 바인딩 했기 때문에 별도 함수 선언은 불필요하다.
+   /*  addString = () => {
         this.props.store.dispatch(add());
-    }
+    } */
 }
+
+let mapDispatchToProps = (dispatch, props) => {
+    console.log('Props from App.js : ' + props.AppProp)
+    return {
+        addString: () => dispatch(add())
+    };
+};
+
+StrAddButton = connect(null, mapDispatchToProps)(StrAddButton);
 
 export default StrAddButton;
