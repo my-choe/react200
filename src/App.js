f19875ecf69554766230d1ad4bbded151a1e3114
my_choe <my_choe@naver.com>
2021-03-24 15:20:14 +0900

    081 react-redux로 스토어 상속하기

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import StrAddButton from './StrAddButton';
 
 /** 
- * props에 스토어를 담아 하위 컴포넌트로 전달하면, 전달받은 컴포넌트에서 스토어에 접근할 수 있다.
- * 컴포넌트에서 dispatch 함수를 사용하면 스토어 데이터를 변경할 수 있다.
+ * redux만 사용해도 충분히 스토어 데이터를 사용하고 변경할 수 있다.
+ * react-redux는 react를 react와 연동해서 사용하기 편리하도록 만든 라이브러리다.
+ * [npm install --save react-redux] 로 설치한다.
  * 
- * props를 통해 index.js에서 전달받은 store에 접근한다.
- * 스토어 state 데이터에서 str 변숫값을 가져온다.
- * 버튼 컴포넌트 StrAddButton에 store를 전달한다.
+ * react-redux의 장점
+ * 1. store를 하위 컴포넌트에 매번 상속하지 않고 사용할 수 있다.
+ * 2. 스토어 데이터를 사용, 변경하는 코드를 모듈화 해 컴포넌트 내에 중복코드사용을 최소화할 수 있다.
+ * 
+ * react-redux도 redux와 마찬가지로 스토어 > 컴포넌트 > 액션 > 리듀서 > 다시 스토어 과정을 통해 데이터를 변경한다.
+ * 차이점은 스토어 > 컴포넌트, 컴포넌트 > 액션 단계에서 connect라는 react-redux 패키지 함수가 사용된다는 것이다.
+ * actions 폴더의 index.js와 reducer 폴더의 index.js파일은 변경 사항이 없다.
+ * 
  */
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,30 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import reducers from './reducers';
 import reportWebVitals from './reportWebVitals';
 
 /**
- * redux는 컨텍스트와 마찬가지로 데이터를 필요한 컴포넌트에서만 요청해 사용할 수 있다.
- * 컨텍스트는 부모 컴포넌트에서 생성한 데이터에 모든 자식 컴포넌트에서 접근할 수 있다.
- * 하지만 redux에서는 컴포넌트 외부의 스토어라는 곳에서 관리한다.
- * 그래서 컴포넌트의 위치에 상관없이 스토어에 접근해 데이터를 사용하고 변경할 수 있다.
- * redux는 데이터를 스토어 > 컴포넌트 > 액션 > 리듀서 > 다시 스토어 과정을 통해 변경한다.
- * [npm install --save redux]로 redux설치
- * 
- * createStore 함수의 파라미터로 reducers 폴더 경로를 넘긴다. 
- * reducers 폴더의 index.js에는 데이터 초깃값을 설정하고 데이터를 변경해주는 함수가 있는데, 이 함수를 '리듀서'라고 한다.
- * 생성한 store를 App 컴포넌트에 전달한다. render 함수를 listener라는 내부함수에 위치시킨다.
- * store를 구독하면 store데이터에 변화가 있을 때 listener 함수 내부의 render함수를 실행하고 변경된 데이터를 렌더링한다.
- * listener() : render함수를 listener함수로 감쌌기 때문에 초기 렌더링을 위해 수동으로 render 함수를 실행시켜준다.
- * 78~80번 실행결과는 80번에서 확인 가능하다.
+ * store상속을 위해 react-redux의 Provider를 import해 사용할 수 있도록 한다.
+ * <Provider> 태그로 App 컴포넌트를 감싸는 부분이 변경됐다.
+ * Provider에 데이터를 넘겨주면 중간 컴포넌트에서 props값을 다시 전달해 줄 필요 없이
+ * 모든 하위 컴포넌트에서 데이터를 사용할 수 있다. 컨텍스트 api에서 사용했던 Provider와 동일기능.
+ * line24는 App 컴포넌트에서 사용할 변수 indexProp에 react문자열을 할당해 props로 전달한다.
+ * 81~83번 실행결과는 83번에서 확인 가능하다.
  */
 
 const store = createStore(reducers); // 스토어 생성함수 import
 
 const listener = () => {
   ReactDOM.render(
-    <App store={store}/>,
+    <Provider store={store}>
+      <App indexProp="react"/>
+    </Provider>,
     document.getElementById('root')
   );
 };
