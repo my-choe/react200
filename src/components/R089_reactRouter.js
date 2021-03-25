7f55a4786f87acdb4459485d908a1b9654b5897c
my_choe <my_choe@naver.com>
2021-03-25 15:17:43 +0900

    090 react-roter-dom Link 사용하기

--- src/components/R089_reactRouter.js
+++ src/components/R089_reactRouter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class R089_reactRouter extends Component {
     render() {
@@ -6,6 +7,7 @@ class R089_reactRouter extends Component {
             <>
                 <h1>path='/'</h1>
                 <h3>R089_reactRouter</h3>
+                <Link to={'/reactRouter2'}>reactRouter2</Link>
             </>
         )
     }
