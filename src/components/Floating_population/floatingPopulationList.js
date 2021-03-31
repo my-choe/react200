12b0f042bd6c5b534570a28269aa4602f9e910ba
my-choe <my_choe@naver.com>
2021-03-31 09:48:26 +0900

    099 서울시 유동인구데이터 사용하기4 - axios로 api 호출하기

--- src/components/Floating_population/floatingPopulationList.js
+++ src/components/Floating_population/floatingPopulationList.js
@@ -1,6 +1,63 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
+/** 2103310부로 Big Data Hub 서비스 종료됨 */
 class floatingPopulationList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            responseFPList: '',
+            append_FPList: '',
+        }
+    }
+
+    componentDidMount() {
+        this.callFloatPopulListApi()
+    }
+
+    callFloatPopulListApi = async () => {
+            axios.get('https://api.bigdatahub.co.kr/v1/datahub/datasets/search.json?pid=1002277&'+
+            'TDCAccessKey=8ce7de5510260c96014c4bb83e3c6bfe967b73fbb24c8e948b846597c395f1eb&$count=30', {
+            })
+            .then( response => {
+                try {
+                    this.setState({ responseFPList: response });
+                    this.setState({ append_FPList: this.FloatPopulListAppend() });
+                } catch (error) {
+                    alert(error)
+                }
+            })
+            .catch( error => {alert(error);return false;} );
+    }
+
+    FloatPopulListAppend = () => {
+        let result = []
+        var FPList = this.state.responseFPList.data
+        var jsonString = JSON.stringify(FPList)
+        jsonString = jsonString.replace(/\(1시간단위\)/g, '')
+        jsonString = jsonString.replace(/\(10세단위\)/g, '')
+        var json = JSON.parse(jsonString)
+
+        for(let i=0; i<json.entry.length; i++){
+            var data = json.entry[i]
+            var idx = i+1
+            result.push(
+                <tr class="hidden_type">
+                    <td>{idx}</td>
+                    <td>{data.일자}</td>
+                    <td>{data.시간}</td>
+                    <td>{data.연령대}</td>
+                    <td>{data.성별}</td>
+                    <td>{data.시}</td>
+                    <td>{data.군구}</td>
+                    <td>{data.유동인구수}</td>
+                </tr>
+            )
+        }
+        return result
+    }
+
     render () {
         return (
             <section class="sub_wrap" >
@@ -22,26 +79,7 @@ class floatingPopulationList extends Component {
                             </tr>
                         </table>	
                         <table class="table_ty2 fp_tlist">
-                            <tr class="hidden_type">
-                                <td>1</td>
-                                <td>20191101</td>
-                                <td>00</td>
-                                <td>40</td>
-                                <td>여성</td>
-                                <td>서울</td>
-                                <td>영등포구</td>
-                                <td>32670</td>
-                            </tr>
-                            <tr class="hidden_type">
-                                <td>1</td>
-                                <td>20191101</td>
-                                <td>00</td>
-                                <td>50</td>
-                                <td>남성</td>
-                                <td>서울</td>
-                                <td>구로구</td>
-                                <td>27888</td>
-                            </tr>
+                            {this.state.append_FPList}
                         </table>
                     </div>
                 </article>
