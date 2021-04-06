f63fb134f8632353df7facc2a9333ae50b327104
my-choe <my_choe@naver.com>
2021-04-06 17:49:43 +0900

    hotfix

--- routes/dbconnect_Module.js
+++ routes/dbconnect_Module.js
@@ -7,7 +7,7 @@ router.use(bodyParser.json());
 
 //mysql 서버 접속 정보
 const connection = mysql.createConnection({
-  host: "react200.cheyrgtsyjfc.us-east-2.rds.amazonaws.com",
+  host: "react200.#####.amazonaws.com",
   port: "3306",
   database: 'react',
   user: "admin",
