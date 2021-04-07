9e46a1e9018c2a8de4901c84eb340bbf10a0c172
my-choe <my_choe@naver.com>
2021-04-07 13:50:13 +0900

    로그인 화면 수정

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import mlogi3 from "../img/main/m_log_i3.png";
+import mlogi2 from "../img/main/m_log_i2.png";
 
 /** 
  * react는 <img> 태그에 require 문법을 사용해 이미지 경로를 할당해야 한다.
@@ -13,18 +15,18 @@ class LoginForm extends Component {
                 <div className="log_box">
                     <form onSubmit={this.handleSubmit}>
                     <div className="in_ty1">
-                        <span><img src={require("../img/main/m_log_i3.png")} alt="" /></span>
+                        <span><img src={mlogi3} alt="" /></span>
                         <input type="text" id="email_val" name="email" placeholder="이메일" onChange={this.handleChange} />
                     </div>
                     <div  className="in_ty1">
-                        <span className="ic_2"><img src={require("../img/main/m_log_i2.png")} alt="" /></span>
+                        <span className="ic_2"><img src={mlogi2} alt="" /></span>
                         <input type="password" id="pwd_val" name="password" placeholder="비밀번호" onChange={this.handleChange} />
                     </div>
+                    <button className="s_bt" type="submit" onClick={this.submitClick}>로그인</button>
                     <ul className="af">
                         <li><Link to={'/register_check'}>회원가입</Link></li>
                         <li className="pwr_b" onClick={this.pwdResetClick}><a href="#n">비밀번호 재설정</a></li>
                     </ul>
-                    <button className="s_bt" type="submit" onClick={this.submitClick}>로그인</button>
                     </form>
                 </div>
                 </div>
